c3817e3c8aa90347ee52ffbf4dc2b188942d1c2b
woajun <woajun@naver.com>
2023-03-19 19:44:19 +0900

    add custom button

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/button.dart';
+
 void main() {
   runApp(const App());
 }
@@ -70,33 +72,15 @@ class App extends StatelessWidget {
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.circular(45)),
-                    child: const Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                      child: Text(
-                        "Transfer",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: const Color(0xFF1F2123),
-                        borderRadius: BorderRadius.circular(45)),
-                    child: const Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                      child: Text(
-                        "Request",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                    ),
-                  ),
+                children: const [
+                  Button(
+                      text: 'Tansfer',
+                      bgColor: Color(0xFFF1B33B),
+                      textColor: Colors.black),
+                  Button(
+                      text: 'Request',
+                      bgColor: Color(0xFF1F2123),
+                      textColor: Colors.white),
                 ],
               )
             ],
