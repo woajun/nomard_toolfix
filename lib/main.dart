6b697962de04e2abbe87e56f8fc644cf823528d0
woajun <woajun@naver.com>
2023-03-19 18:09:16 +0900

    add Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,43 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Hello flutter!'),
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
           ),
-          body: Center(
-            child: Text('Hello World!'),
-          )),
+        ),
+      ),
     );
   }
 }
