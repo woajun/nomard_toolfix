43c5be888908caf789ce8c305dbf28bbe707c3fb
woajun <woajun@naver.com>
2023-03-19 18:53:51 +0900

    add Total Balance

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,11 @@ class App extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color(0xFF181818),
         body: Padding(
-          padding: EdgeInsets.all(10),
+          padding: EdgeInsets.symmetric(
+            horizontal: 40,
+          ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 80,
@@ -39,7 +42,45 @@ class App extends StatelessWidget {
                         ),
                       ),
                     ],
-                  )
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                    fontSize: 22, color: Colors.white.withOpacity(0.8)),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                '\$5 194 482',
+                style: TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(45)),
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                      child: Text(
+                        "Transfer",
+                        style: TextStyle(fontSize: 22),
+                      ),
+                    ),
+                  ),
                 ],
               )
             ],
