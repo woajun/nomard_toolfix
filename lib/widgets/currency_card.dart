c6eb5e734017ebec41331dece278183d30d5858b
woajun <woajun@naver.com>
2023-03-21 23:55:16 +0900

    add card & use SingleChildScrollView

---
+++ lib/widgets/currency_card.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class CurrencyCard extends StatelessWidget {
+  final String name, code, amount;
+  final IconData icon;
+  final bool isInverted;
+
+  final _blackColor = const Color(0xFF1f2123);
+
+  const CurrencyCard({
+    super.key,
+    required this.name,
+    required this.amount,
+    required this.icon,
+    required this.code,
+    required this.isInverted,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var textColor = isInverted ? _blackColor : Colors.white;
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        color: isInverted ? Colors.white : _blackColor,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(
+                    color: textColor,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      amount,
+                      style: TextStyle(
+                        color: textColor,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      code,
+                      style: TextStyle(
+                        color: textColor.withOpacity(0.8),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Transform.scale(
+              scale: 2.2,
+              child: Transform.translate(
+                offset: const Offset(
+                  -5,
+                  12,
+                ),
+                child: Icon(
+                  icon,
+                  color: textColor,
+                  size: 88,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
